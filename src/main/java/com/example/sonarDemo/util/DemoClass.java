7be7f57f233ed38c01d1c403e5820a45c453df65
vn56c5j <junyi.Lin@walmart.com>
2023-10-19 21:03:28 -0400

    first commit

---
+++ src/main/java/com/example/sonarDemo/util/DemoClass.java
@@ -0,0 +1,12 @@
+package com.example.sonarDemo.util;
+
+public class DemoClass {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
